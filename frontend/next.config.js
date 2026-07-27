304006623323defa00b0e6d8b295d09ba6d8b03b
thakkou <taha.hakkou42@gmail.com>
2026-07-27 10:59:33 +0100

    Resolved bugs (env vars without quotes + volume for uploads + frontend ip AUTH_URL)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,25 +6,25 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
-    output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
-    images: {
-      remotePatterns: [
-        {
-          protocol: "http",
-          hostname: "**",
-        },
-        {
-          protocol: "https",
-          hostname: "**",
-        },
-      ],
-    },
-    eslint: {
-      // This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-      ignoreDuringBuilds: true,
-    },
-    async rewrites() {
+  output: process.env.NODE_ENV === "production" ? "standalone" : undefined,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  eslint: {
+    // This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  async rewrites() {
     return [
       {
         source: "/uploads/:path*",
